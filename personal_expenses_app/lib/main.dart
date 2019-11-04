f8dec23308013877e15dabc8365458d54868e0b8
AhmedMostafaElbasha <ahmed2elbasha@gmail.com>
2019-11-04 16:48:11 +0200

    Added a Transcation Model and a Transaction Data

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Weekly Groceries",
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -30,8 +46,8 @@ class MyHomePage extends StatelessWidget {
             child: Card(
               color: Colors.blue,
               child: Text(
-                  'CHART!',
-                ),
+                'CHART!',
+              ),
               elevation: 5,
             ),
           ),
