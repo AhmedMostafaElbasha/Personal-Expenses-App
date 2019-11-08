7e2c3fadd92f50bf604dd445f8a898b92b70d51c
AhmedMostafaElbasha <ahmed2elbasha@gmail.com>
2019-11-08 21:10:05 +0200

    Made the UI Interface Scrollable

--- personal_expenses_app/lib/widgets/transaction_list.dart
+++ personal_expenses_app/lib/widgets/transaction_list.dart
@@ -6,58 +6,60 @@ class TransactionList extends StatelessWidget {
   final List<Transaction> userTransctions;
 
   TransactionList({@required this.userTransctions});
-  
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransctions.map(
-        (transaction) {
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
           return Card(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  )),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    '\$${transaction.amount}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      transaction.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
                     ),
-                    Text(
-                      DateFormat.yMMMd().format(transaction.date),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${userTransctions[index].amount}',
                       style: TextStyle(
-                        color: Colors.grey,
-                      ),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
                     ),
-                  ],
-                )
-              ],
-            ),
-          );
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        userTransctions[index].title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(userTransctions[index].date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ); 
         },
-      ).toList(),
+        itemCount: userTransctions.length,
+      ),
     );
   }
 }
